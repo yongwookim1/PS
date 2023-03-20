324e187cd11b483454e2414781782655a17a9c03
yongwoo <kyw1654@gmial.com>
2023-03-20 17:21:38 +0900

    made more simply

--- hotelACM.py
+++ hotelACM.py
@@ -2,10 +2,6 @@ N = int(input())
 
 for i in range(N):
     h, w, n = map(int,input().split())
-    if n % h == 0:
-        y = h
-        x = n // h
-    else:
-        y = n % h
-        x = (n // h) + 1
+    y = ((n-1) % h) + 1
+    x = ((n-1) // h) + 1
     print(f"{y}{x:02d}")
