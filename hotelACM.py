85cb6d373471316e7cf74c1afd08571a54dab825
yongwoo <113939970+yongwookim1@users.noreply.github.com>
2023-03-20 14:30:46 +0900

    changed condition in boundary

--- hotelACM.py
+++ hotelACM.py
@@ -2,6 +2,10 @@ N = int(input())
 
 for i in range(N):
     h, w, n = map(int,input().split())
-    y = n % h
-    x = (n // h) + 1
+    if n % h == 0:
+        y = h
+        x = n // h
+    else:
+        y = n % h
+        x = (n // h) + 1
     print(f"{y}{x:02d}")
