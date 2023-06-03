25b2378428a0f722bd2fce1c5c31bceb34bde763
yongwoo <kyw1654@gmail.com>
2023-06-03 18:24:38 +0900

    [Silver IV] Title: 동전 0, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -3,14 +3,11 @@ n, k = map(int, input().split())
 coin = []
 for i in range(n):
     coin.append(int(input()))
+coin.sort(reverse=True)
 
 st = 0
-for i in coin[::-1]:
-    if k >= i:
-        while True:
-            if k < i:
-                break
-            k -= i
-            st += 1
+for i in coin:
+    st += k // i
+    k = k % i
 
 print(st)
