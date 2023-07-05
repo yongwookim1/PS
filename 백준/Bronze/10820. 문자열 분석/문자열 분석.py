f7416b88390a16af347785593ff419c37392f5f8
yongwoo <kyw1654@gmail.com>
2023-07-05 10:05:23 +0900

    [Bronze II] Title: 문자열 분석, Time: 132 ms, Memory: 114488 KB -BaekjoonHub

--- 백준/Bronze/10820. 문자열 분석/문자열 분석.py
+++ 백준/Bronze/10820. 문자열 분석/문자열 분석.py
@@ -1,21 +1,16 @@
-import sys
-
 while True:
-    line = sys.stdin.readline().rstrip('\n')
-
-    if not line:
+    try:
+        a = input()
+        l, u , d, b = 0, 0, 0, 0
+        for i in a:
+            if i.islower():
+                l += 1
+            elif i.isupper():
+                u += 1
+            elif i.isdigit():
+                d += 1
+            else:
+                b += 1
+        print(l, u, d, b)
+    except EOFError:
         break
-
-    # 소문자, 대문자, 숫자, 공백
-    l, u, d, s = 0, 0, 0, 0
-    for each in line:
-        if each.islower():
-            l += 1
-        elif each.isupper():
-            u += 1
-        elif each.isdigit():
-            d += 1
-        elif each.isspace():
-            s += 1
-
-    print(l, u, d, s)
