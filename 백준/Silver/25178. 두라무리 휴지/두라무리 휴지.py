db06fdd07c5c0d138e9d70da2a7f82406f57b2c1
yongwoo <kyw1654@gmail.com>
2023-07-10 22:35:46 +0900

    [Silver V] Title: 두라무리 휴지, Time: 168 ms, Memory: 123872 KB -BaekjoonHub

--- 백준/Silver/25178. 두라무리 휴지/두라무리 휴지.py
+++ 백준/Silver/25178. 두라무리 휴지/두라무리 휴지.py
@@ -1,14 +1,8 @@
-n = int(input())
-a = input()
-b = input()
-
-if sorted(a) == sorted(b) and a[0] == b[0] and a[-1] == b[-1]:
+n=int(input())
+a=input();b=input()
+if sorted(a)==sorted(b) and a[0]==b[0] and a[-1]==b[-1]:
     for i in 'aeiou':
-        a = a.replace(i,'')
-        b = b.replace(i,'')
-    if a==b:
-        print("YES")
-    else:
-        print("NO")
+        a=a.replace(i,'');b=b.replace(i,'')
+    print("YES" if a==b else "NO")
 else:
     print("NO")
