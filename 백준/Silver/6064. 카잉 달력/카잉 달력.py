78e25f115b7fc1082b97f861133e7bf3e9667f4e
Yongwoo Kim <kyw1654@gmail.com>
2023-11-18 01:02:10 +0900

    [Silver I] Title: 카잉 달력, Time: 264 ms, Memory: 110592 KB -BaekjoonHub

--- 백준/Silver/6064. 카잉 달력/카잉 달력.py
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.py
@@ -8,17 +8,8 @@ for i in range(t):
     M, N, x, y = map(int, input().split())
 
     for i in range(x, M * N // math.gcd(M, N) + 1, M):
-        if i % M == 0:
-            p = M
-        else:
-            p = i % M
-        if i % N == 0:
-            q = N
-        else:
-            q = i % N
-        if p == x and q == y:
+        if (i - y) % N == 0:
             print(i)
-
             break
     else:
         print(-1)
