8ba1a4f5a1ac187393a40891fe4c87a1ac958c35
Yongwoo Kim <kyw1654@gmail.com>
2024-01-15 18:44:29 +0900

    [Gold IV] Title: 치즈, Time: 176 ms, Memory: 115148 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.py
+++ 백준/Gold/2636. 치즈/치즈.py
@@ -1,76 +1,47 @@
 from collections import deque
-import sys
+import time
 
-input = sys.stdin.readline
+n, m = map(int, input().split())
 
-n, m = map(int, input().rstrip().split())
-
-graph = [list(map(int, input().rstrip().split())) for _ in range(n)]
+graph = [list(map(int, input().split())) for _ in range(n)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-time = 0
-while True:
-    result = [element for sublist in graph for element in sublist]
-    if all(element == 0 for element in result):
-        print(time)
-        print(left_cheeze)
-        break
 
-    def find_the_hole(queue, graph):
-        while queue:
-            x, y = queue.popleft()
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
-                    graph[nx][ny] = 2
-                    queue.append((nx, ny))
-
-    gone = []
-
-    def bfs(queue, graph):
-        st = 0
-        while queue:
-            x, y = queue.popleft()
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
-                    st += 1
-                    break
-            if st >= 1:
-                gone.append((x, y))
-
-    queue = deque()
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j] == 0:
-                queue.append((i, j))
-                find_the_hole(queue, graph)
-
-    queue.append((0, 0))
-    graph[0][0] = 0
+def bfs(queue, visited):
+    queue2 = deque()
     while queue:
         x, y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 2:
-                graph[nx][ny] = 0
-                queue.append((nx, ny))
+            if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] == False:
+                if graph[nx][ny] == 0:
+                    visited[nx][ny] = True
+                    queue.append((nx, ny))
+                elif graph[nx][ny] == 1:
+                    visited[nx][ny] = True
+                    queue2.append((nx, ny))
 
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j] == 1:
-                queue.append((i, j))
-                bfs(queue, graph)
+    for x, y in queue2:
+        graph[x][y] = 0
 
-    left_cheeze_list = [element for sublist in graph for element in sublist]
-    left_cheeze = len([True for element in left_cheeze_list if element == 1])
+    return queue2
 
-    for p, q in gone:
-        graph[p][q] = 0
 
-    time += 1
+queue = deque()
+queue.append((0, 0))
+t = 0
+while True:
+    result = [element for sublist in graph for element in sublist]
+    if all(element == 0 for element in result):
+        print(t)
+        print(len_left_cheeze)
+        break
+
+    visited = [[False] * m for _ in range(n)]
+    left_cheeze = [element for sublist in graph for element in sublist]
+    len_left_cheeze = len([i for i in left_cheeze if i == 1])
+    queue = bfs(queue, visited)
+    t += 1
