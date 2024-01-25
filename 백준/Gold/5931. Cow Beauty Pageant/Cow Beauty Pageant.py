5e6caf205a2584e30cadd4523c2f062d4bed2600
Yongwoo Kim <kyw1654@gmail.com>
2024-01-26 02:44:55 +0900

    [Gold V] Title: Cow Beauty Pageant, Time: 160 ms, Memory: 114268 KB -BaekjoonHub

--- 백준/Gold/5931. Cow Beauty Pageant/Cow Beauty Pageant.py
+++ 백준/Gold/5931. Cow Beauty Pageant/Cow Beauty Pageant.py
@@ -1,49 +1,65 @@
 from collections import deque
-from copy import deepcopy
 
-def bfs(y, x, n):
-    q = deque()
-    q.append((y, x))
-    graph[y][x] = n
-    while q:
-        y, x = q.popleft()
-        for dy, dx in d:
-            Y, X = y+dy, x+dx
-            if (0 <= Y < N) and (0 <= X < M) and graph[Y][X] == 'X':
-                q.append((Y, X))
-                graph[Y][X] = n
-                
-def bfs2(y, x):
-    q = deque()
-    q.append((y, x))
-    a[y][x] = 0
-    while q:
-        y, x = q.popleft()
-        for dy, dx in d:
-            Y, X = y+dy, x+dx
-            if (0 <= Y < N) and (0 <= X < M):
-                if a[Y][X] == '2':
-                    return a[y][x]
-                if a[Y][X] == '.':
-                    q.append((Y, X))
-                    a[Y][X] = a[y][x]+1
+
+n, m = map(int, input().split())
+
+graph = [list(input()) for _ in range(n)]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def divide(queue):
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m:
+                if graph[nx][ny] == "X":
+                    graph[nx][ny] = "O"
+                    queue.append((nx, ny))
+
+
+flag = False
+for i in range(n):
+    if flag == True:
+        break
+    for j in range(m):
+        if graph[i][j] == "X":
+            queue = deque()
+            queue.append((i, j))
+            graph[i][j] = "O"
+            divide(queue)
+            flag = True
+            break
+
+
+def root(queue, visited):
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] == 0:
+                if graph[nx][ny] == ".":
+                    visited[nx][ny] = visited[x][y] + 1
+                    queue.append((nx, ny))
+                elif graph[nx][ny] == "X":
+                    return visited[x][y]
     return -1
 
-N, M = map(int, input().split())
-graph = [list(input()) for _ in range(N)]
-d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-cnt = 1
-for i in range(N):
-    for j in range(M):
-        if graph[i][j] == 'X':
-            bfs(i, j, str(cnt))
-            cnt += 1
-res = N*M
-for i in range(N):
-    for j in range(M):
-        if graph[i][j] == '1':
-            a = deepcopy(graph)
-            t = bfs2(i, j)
-            if t > -1:
-                res = min(res, t)
-print(res)
+
+mn = m * n
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == "O":
+            queue = deque()
+            queue.append((i, j))
+            visited = [[0] * m for _ in range(n)]
+            r = root(queue, visited)
+            if r > -1:
+                if r <= mn:
+                    mn = r
+
+print(mn)
