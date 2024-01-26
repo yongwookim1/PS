def1b6963ed28351f0483959b2e0a9e5119ffb56
Yongwoo Kim <kyw1654@gmail.com>
2024-01-27 00:31:30 +0900

    [Gold V] Title: 농장 관리, Time: 180 ms, Memory: 114212 KB -BaekjoonHub

--- 백준/Gold/1245. 농장 관리/농장 관리.py
+++ 백준/Gold/1245. 농장 관리/농장 관리.py
@@ -10,35 +10,31 @@ graph = [list(map(int, input().strip().split())) for _ in range(n)]
 dx = [-1, -1, -1, 0, 0, 1, 1, 1]
 dy = [-1, 0, 1, 1, -1, -1, 0, 1]
 
-visited1 = [[False] * m for _ in range(n)]
 
-
-def bfs(queue, visited1, visited2, h):
+def bfs(queue, h):
+    r = 1
     while queue:
         x, y = queue.popleft()
         for i in range(8):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and visited2[nx][ny] == False:
-                if graph[nx][ny] > h:
-                    return 0
-                elif graph[nx][ny] == h:
-                    visited1[nx][ny] = True
-                    visited2[nx][ny] = True
+            if 0 <= nx < n and 0 <= ny < m:
+                if graph[nx][ny] == h and visited[nx][ny] == False:
+                    visited[nx][ny] = True
                     queue.append((nx, ny))
-                elif graph[nx][ny] == h - 1:
-                    continue
-    return 1
+                elif graph[nx][ny] > h:
+                    r = 0
+    return r
 
 
+visited = [[False] * m for _ in range(n)]
 st = 0
 for i in range(n):
     for j in range(m):
-        if visited1[i][j] == False and graph[i][j] != 0:
+        if graph[i][j] != 0 and visited[i][j] == False:
             queue = deque()
             queue.append((i, j))
-            visited1[i][j] = True
-            visited2 = [[False] * m for _ in range(n)]
-            visited2[i][j] = True
-            st += bfs(queue, visited1, visited2, graph[i][j])
+            visited[i][j] = True
+            st += bfs(queue, graph[i][j])
+
 print(st)
