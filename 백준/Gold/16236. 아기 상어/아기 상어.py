9904192317d0b63475b8c4e4152913500adc1a84
Yongwoo Kim <kyw1654@gmail.com>
2024-03-07 21:55:39 +0900

    [Gold III] Title: 아기 상어, Time: 184 ms, Memory: 114948 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,31 +1,16 @@
 from collections import deque
 
-n = int(input())
 
-graph = []
-queue = deque()
-for i in range(n):
-    graph.append(list(map(int, input().split())))
-    for j in range(n):
-        if graph[i][j] == 9:
-            queue.append((i, j, 0, 2, 0))
-            graph[i][j] = 0
-            break
-
-dx = [0, -1, 1, 0]
-dy = [-1, 0, 0, 1]
-
-
-def bfs(queue: deque, visited: list):
+def bfs(queue: deque, visited: list, n: int, graph: list):
     flag = False
     tq = deque()
     while queue:
         y, x, t, l, eat = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < n and visited[ny][nx] == False:
-                if graph[ny][nx] == 0:
+        for dx, dy in [(0, -1), (-1, 0), (1, 0), (0, 1)]:
+            nx = x + dx
+            ny = y + dy
+            if 0 <= nx < n and 0 <= ny < n and not visited[ny][nx]:
+                if graph[ny][nx] == 0 or graph[ny][nx] == l:
                     visited[ny][nx] = True
                     queue.append((ny, nx, t + 1, l, eat))
                 elif graph[ny][nx] < l:
@@ -33,29 +18,37 @@ def bfs(queue: deque, visited: list):
                         tq.append((ny, nx, t + 1, l, eat + 1))
                     elif eat + 1 == l:
                         tq.append((ny, nx, t + 1, l + 1, 0))
-                    result = t + 1
                     flag = True
-                elif graph[ny][nx] == l:
-                    visited[ny][nx] = True
-                    queue.append((ny, nx, t + 1, l, eat))
-    if flag:
-        return tq
-    else:
-        return False
+    return tq if flag else None
 
 
-res = 0
-while True:
-    visited = [[False] * n for _ in range(n)]
-    visited[queue[0][0]][queue[0][1]] = True
-    r = bfs(queue, visited)
-    if r == False:
-        print(res)
-        break
-    else:
-        tq = r
-        tq = list(tq)
-        tq.sort(key=lambda x: [x[2], x[0], x[1]])
-        queue.append(tq[0])
-        graph[queue[0][0]][queue[0][1]] = 0
-        res = queue[0][2]
+def main():
+    n = int(input())
+
+    graph = []
+    queue = deque()
+    for i in range(n):
+        graph.append(list(map(int, input().split())))
+        for j in range(n):
+            if graph[i][j] == 9:
+                queue.append((i, j, 0, 2, 0))
+                graph[i][j] = 0
+                break
+
+    res = 0
+    while True:
+        visited = [[False] * n for _ in range(n)]
+        visited[queue[0][0]][queue[0][1]] = True
+        r = bfs(queue, visited, n, graph)
+        if not r:
+            print(res)
+            break
+        else:
+            tq = sorted(r, key=lambda x: [x[2], x[0], x[1]])
+            queue.append(tq[0])
+            graph[queue[0][0]][queue[0][1]] = 0
+            res = queue[0][2]
+
+
+if __name__ == "__main__":
+    main()
