b779faf6c0fab99de9c9435612abd68f1c1ef3e6
Yongwoo Kim <kyw1654@gmail.com>
2024-03-11 14:38:47 +0900

    [Gold II] Title: 스타트 택시, Time: 308 ms, Memory: 115872 KB -BaekjoonHub

--- 백준/Gold/19238. 스타트 택시/스타트 택시.py
+++ 백준/Gold/19238. 스타트 택시/스타트 택시.py
@@ -15,12 +15,12 @@ queue = deque()
 queue.append((s1 - 1, s2 - 1, 0, f))
 
 
-def go_to_sp(queue, visited):
+def go_to_sp(queue: deque, visited: list):
     tq = deque()
     lim = 500000
     while queue:
         y, x, d, f = queue.popleft()
-        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1), (0, 0)]:
+        for dx, dy in [(0, 0), (-1, 0), (1, 0), (0, -1), (0, 1)]:
             nx, ny = x + dx, y + dy
             if (
                 0 <= nx < n
@@ -29,61 +29,54 @@ def go_to_sp(queue, visited):
                 and f > 0
                 and d <= lim
             ):
-                if (
-                    graph[ny][nx] == 0
-                    and (ny, nx) in start_end.keys()
-                    and dx == 0
-                    and dy == 0
-                ):
-                    visited[ny][nx] = True
-                    queue.append((ny, nx, d, f))
-                    tq.append((ny, nx, d, f, 0))
-                    lim = d + 1
-                elif graph[ny][nx] == 0 and (ny, nx) in start_end.keys():
-                    visited[ny][nx] = True
-                    queue.append((ny, nx, d + 1, f - 1))
-                    tq.append((ny, nx, d + 1, f - 1, 0))
-                    lim = d + 1
-                if graph[ny][nx] == 0 and dx == 0 and dy == 0:
-                    visited[ny][nx] = True
-                    queue.append((ny, nx, d, f))
-                if graph[ny][nx] == 0:
-                    visited[ny][nx] = True
-                    queue.append((ny, nx, d + 1, f - 1))
+                visited[ny][nx] = True
+                if dx == 0 and dy == 0:
+                    if graph[ny][nx] == 0 and (ny, nx) in start_end.keys():
+                        queue.append((ny, nx, d, f))
+                        tq.append((ny, nx, d, f, 0))
+                        lim = d + 1
+                    elif graph[ny][nx] == 0:
+                        queue.append((ny, nx, d, f))
+                else:
+                    if graph[ny][nx] == 0 and (ny, nx) in start_end.keys():
+                        queue.append((ny, nx, d + 1, f - 1))
+                        tq.append((ny, nx, d + 1, f - 1, 0))
+                        lim = d + 1
+                    elif graph[ny][nx] == 0:
+                        queue.append((ny, nx, d + 1, f - 1))
+
     if tq:
         tq = sorted(tq, key=lambda x: [x[2], x[0], x[1]])
         queue = deque()
         queue.append(tq[0])
-        idx = (queue[0][0], queue[0][1])
-        ep = start_end[idx]
-        del start_end[idx]
+        ep = start_end[(queue[0][0], queue[0][1])]
+        del start_end[(queue[0][0], queue[0][1])]
         return queue, ep
     else:
         return False
 
 
-def go_to_ep(queue, visited, ep):
+def go_to_ep(queue: deque, visited: list, ep: tuple):
     while queue:
         y, x, d, f, af = queue.popleft()
-        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1), (0, 0)]:
+        for dx, dy in [(0, 0), (-1, 0), (1, 0), (0, -1), (0, 1)]:
             nx, ny = x + dx, y + dy
             if 0 <= nx < n and 0 <= ny < n and visited[ny][nx] == False and f > 0:
-                if graph[ny][nx] == 0 and ep == (ny, nx) and dx == 0 and dy == 0:
-                    visited[ny][nx] = True
-                    queue = deque()
-                    queue.append((ny, nx, d, f + ((af + 1) * 2)))
-                    return queue
-                elif graph[ny][nx] == 0 and ep == (ny, nx):
-                    visited[ny][nx] = True
-                    queue = deque()
-                    queue.append((ny, nx, d + 1, f - 1 + ((af + 1) * 2)))
-                    return queue
-                if graph[ny][nx] == 0 and dx == 0 and dy == 0:
-                    visited[ny][nx] = True
-                    queue.append((ny, nx, d, f, af))
-                elif graph[ny][nx] == 0:
-                    visited[ny][nx] = True
-                    queue.append((ny, nx, d + 1, f - 1, af + 1))
+                visited[ny][nx] = True
+                if dx == 0 and dy == 0:
+                    if graph[ny][nx] == 0 and ep == (ny, nx):
+                        queue = deque()
+                        queue.append((ny, nx, d, f + ((af + 1) * 2)))
+                        return queue
+                    if graph[ny][nx] == 0:
+                        queue.append((ny, nx, d, f, af))
+                else:
+                    if graph[ny][nx] == 0 and ep == (ny, nx):
+                        queue = deque()
+                        queue.append((ny, nx, d + 1, f - 1 + ((af + 1) * 2)))
+                        return queue
+                    elif graph[ny][nx] == 0:
+                        queue.append((ny, nx, d + 1, f - 1, af + 1))
     return False
 
 
