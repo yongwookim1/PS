6e301e1d581ff03a223e3ab28811e5b1e2243c4c
yongwookim1 <kyw1654@gmail.com>
2024-06-04 17:03:34 +0900

    delete some files

--- hotelACM.py
+++
@@ -1,7 +0,0 @@
-N = int(input())
-
-for i in range(N):
-    h, w, n = map(int,input().split())
-    y = ((n-1) % h) + 1
-    x = ((n-1) // h) + 1
-    print(f"{y}{x:02d}")
--- plusandminus.py
+++
@@ -1,7 +0,0 @@
-a, b = map(int,input().split())
-s = 1
-
-for i in range(a,b+1):
-    s *= sum([i for i in range(1,i+1)])
-
-print(s%14579)
